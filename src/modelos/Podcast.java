896bc80c91ec6dbf6142f2dd46ab1c7e7a70b105
Wesleyy06 <wesleydesousa34@gmail.com>
2025-03-03 19:58:09 -0300

    Implementação da classe Podcast

---
+++ src/modelos/Podcast.java
@@ -0,0 +1,22 @@
+package modelos;
+
+public class Podcast {
+    private String apresentador;
+    private String descricaoEpisodio;
+
+    public String getApresentador() {
+        return apresentador;
+    }
+
+    public void setApresentador(String apresentador) {
+        this.apresentador = apresentador;
+    }
+
+    public String getDescricaoEpisodio() {
+        return descricaoEpisodio;
+    }
+
+    public void setDescricaoEpisodio(String descricaoEpisodio) {
+        this.descricaoEpisodio = descricaoEpisodio;
+    }
+}
