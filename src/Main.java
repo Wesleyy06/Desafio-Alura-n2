7354acfe6fd4382ce5b531ec4975d3352c1551e5
Wesleyy06 <wesleydesousa34@gmail.com>
2025-03-03 20:07:03 -0300

    Implantação de algumas instancias da classe Musica.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import modelos.Musica;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Como tudo deve ser");
+        minhaMusica.setBanda("Charlie Brown Jr");
+        minhaMusica.setGenero("Pop rock");
+
+        for (int i = 0; i < 100; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 40; i++) {
+            minhaMusica.curtir();
+        }
+
     }
 }
--- src/modelos/Musica.java
+++ src/modelos/Musica.java
@@ -4,4 +4,28 @@ public class Musica extends Audio{
     private String banda;
     private String genero;
     private String albun;
+
+    public String getBanda() {
+        return banda;
+    }
+
+    public void setBanda(String banda) {
+        this.banda = banda;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAlbun() {
+        return albun;
+    }
+
+    public void setAlbun(String albun) {
+        this.albun = albun;
+    }
 }
